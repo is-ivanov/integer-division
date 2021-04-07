8ca1a470054b044e9bdefaddd1743d29f12568b9
Ivan Ivanou <ii99xt1@gmail.com>
2021-04-07 18:03:47 +0300

    add test dividend==0 and dividend==divisor

--- src/test/java/ua/com/foxminded/integerdivision/DivisionCalculatorTest.java
+++ src/test/java/ua/com/foxminded/integerdivision/DivisionCalculatorTest.java
@@ -13,24 +13,24 @@ class DivisionCalculatorTest {
     DivisionCalculator calculator;
     ResultOfCalculations actual;
     ResultOfCalculations result;
+    List <DivisionStep> stepsOfCalculate; 
     
     @BeforeEach
     void setUp() throws Exception {
         calculator = new DivisionCalculator();
+        stepsOfCalculate = new ArrayList<>();
     }
 
     @Test
     @DisplayName("test input data from task4 example should output data from task4")
-    void testCalculateResult() {
+    void testCalculateResultDataFromTask4() {
         actual = calculator.calculateResult(78945, 4);
         
-        List <DivisionStep> stepsOfCalculate = new ArrayList<>();
         stepsOfCalculate.add(new DivisionStep(7, 4, 3));
         stepsOfCalculate.add(new DivisionStep(38, 36, 2));
         stepsOfCalculate.add(new DivisionStep(29, 28, 1));
         stepsOfCalculate.add(new DivisionStep(14, 12, 2));
         stepsOfCalculate.add(new DivisionStep(25, 24, 1));
-        
         
         result = new ResultOfCalculations.ResultOfCalculationsBuilder(78945, 4)
                 .quotient(19736)
@@ -38,6 +38,37 @@ class DivisionCalculatorTest {
                 .divisionSteps(stepsOfCalculate)
                 .build();
         assertEquals(result, actual);
+    }
+    
+    @Test
+    @DisplayName("test dividend equals divisor")
+    void testCalculateResult_DividentEqualsDivisor() {
+        actual = calculator.calculateResult(4578, 4578);
+        stepsOfCalculate.add(new DivisionStep(4, 0, 4));
+        stepsOfCalculate.add(new DivisionStep(45, 0, 45));
+        stepsOfCalculate.add(new DivisionStep(457, 0, 457));
+        stepsOfCalculate.add(new DivisionStep(4578, 4578, 0));
+        
+        result = new ResultOfCalculations.ResultOfCalculationsBuilder(4578, 4578)
+                .quotient(1)
+                .remainder(0)
+                .divisionSteps(stepsOfCalculate)
+                .build();
+        assertEquals(result.toString(), actual.toString());
+    }
+    
+    @Test
+    @DisplayName("test dividend equals zero")
+    void testCalculateResult_DividentEqualsZero() {
+        actual = calculator.calculateResult(0, 4578);
+        stepsOfCalculate.add(new DivisionStep(0, 0, 0));
+        
+        result = new ResultOfCalculations.ResultOfCalculationsBuilder(0, 4578)
+                .quotient(0)
+                .remainder(0)
+                .divisionSteps(stepsOfCalculate)
+                .build();
+        assertEquals(result.toString(), actual.toString());
     }
 
 }
