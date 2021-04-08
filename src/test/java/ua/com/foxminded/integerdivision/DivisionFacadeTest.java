66c2ab832e4987e88b1f98149d7914f07fff6401
Ivan Ivanou <ii99xt1@gmail.com>
2021-04-08 23:57:01 +0300

    fix tests Facade

--- src/test/java/ua/com/foxminded/integerdivision/DivisionFacadeTest.java
+++ src/test/java/ua/com/foxminded/integerdivision/DivisionFacadeTest.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -21,54 +22,83 @@ class DivisionFacadeTest {
     @Mock
     DivisionCalculator calculatorMock;
     @Mock
-    Formatable divisionFormatterMock;
-    @Mock 
+    Formatable formatterMock;
+    @Mock
     ResultOfCalculations result;
 
     @BeforeEach
     void setUp() {
-//        calculatorMock = mock(DivisionCalculator.class);
-//        divisionFormatterMock = mock(DivisionFormatter.class);
-        divisionFacade = new DivisionFacade(calculatorMock, divisionFormatterMock);
+        divisionFacade = new DivisionFacade(calculatorMock,
+                formatterMock);
     }
 
     @Test
-    @DisplayName("test input zero to divisor should output exception")
-    void testDivisorIsZero() {
+    @DisplayName("test case: divisor equals zero -> Facade should throw IllegalArgumentException with right message")
+    void testDivisorEqualsZero_ThrowException() {
         Exception exception = assertThrows(IllegalArgumentException.class,
                 () -> divisionFacade.start(15, 0));
         assertEquals(MESSAGE_DIVIDE_BY_ZERO, exception.getMessage());
     }
-    
-    @Test
-    @DisplayName("test input zero to dividend should call calculator and then formatter")
-    void testDividendIsZero() {
-        calculatorMock.calculateResult(0, 15);
-        verify(calculatorMock).calculateResult(0, 15);
         
-        doReturn(result).when(calculatorMock).calculateResult(0, 15);
+    @Nested
+    @DisplayName("test case: dividend equals zero")
+    class DividendEqualsZero {
+
+        @Test
+        @DisplayName("Facade should call calculator with right arguments")
+        void testDividendEqualsZero_CallCalculator() {
+            divisionFacade.start(0, 15);
+            verify(calculatorMock).calculateResult(0, 15);
+        }
         
-        divisionFormatterMock.formatLine(result);
-        verify(divisionFormatterMock).formatLine(result);
+        @Test
+        @DisplayName("Facade should call formatter after calculator with right object")
+        void testDividendEqualsZero_CallFormatterAfterCalculator() {
+            doReturn(result).when(calculatorMock).calculateResult(0, 15);
+            divisionFacade.start(0, 15);
+            verify(formatterMock).formatLine(result);
+        }
         
-        when(divisionFormatterMock.formatLine(result)).thenReturn(RESULT_DIVISION_ZERO_DIVIDEND);
-        assertEquals(RESULT_DIVISION_ZERO_DIVIDEND, divisionFacade.start(0, 15));
+        @Test
+        @DisplayName("Facade should output right string from formatter")
+        void testDividendEqualsZero_RightOutput () {
+            doReturn(result).when(calculatorMock).calculateResult(0, 15);
+            when(formatterMock.formatLine(result)).thenReturn(RESULT_DIVISION_ZERO_DIVIDEND);
+            
+            assertEquals(RESULT_DIVISION_ZERO_DIVIDEND, divisionFacade.start(0, 15));
+        }
+        
     }
-    
-    @Test
-    @DisplayName("test input two numerals should call calculator and then formatter")
-    void testNormalDivision() {
-        calculatorMock.calculateResult(345, 15);
-        verify(calculatorMock).calculateResult(345, 15);
+
+    @Nested
+    @DisplayName("test case: normal division")
+    class NormalDivision {
+
+        @Test
+        @DisplayName("Facade should call calculator with right arguments")
+        void testNormalDivision_CallCalculator() {
+            divisionFacade.start(17895, 15);
+            verify(calculatorMock).calculateResult(17895, 15);
+        }
         
-        doReturn(result).when(calculatorMock).calculateResult(345, 15);
+        @Test
+        @DisplayName("Facade should call formatter after calculator with right object")
+        void testNormalDivision_CallFormatterAfterCalculator() {
+            doReturn(result).when(calculatorMock).calculateResult(78965, 745);
+            divisionFacade.start(78965, 745);
+            verify(formatterMock).formatLine(result);
+        }
         
-        divisionFormatterMock.formatLine(result);
-        verify(divisionFormatterMock).formatLine(result);
+        @Test
+        @DisplayName("Facade should output right string from formatter")
+        void testNormalDivision_RightOutput () {
+            doReturn(result).when(calculatorMock).calculateResult(457, 15);
+            when(formatterMock.formatLine(result)).thenReturn(RESULT_DIVISION);
+            
+            assertEquals(RESULT_DIVISION, divisionFacade.start(457, 15));
+        }
         
-        when(divisionFormatterMock.formatLine(result)).thenReturn(RESULT_DIVISION);
-        assertEquals(RESULT_DIVISION, divisionFacade.start(345, 15));
     }
-    
+
 
 }
